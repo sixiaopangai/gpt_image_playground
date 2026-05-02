99391e1c71f0cde501fcce37df8e16c852b1a70e
sixiaopangai <2904153061@qq.com>
2026-05-02 13:57:58 +0800

    Update help repository branding

--- src/siteConfig.ts
+++ src/siteConfig.ts
@@ -3,6 +3,7 @@ export const SITE_CONFIG = {
   shortName: '十二少AI绘图坊',
   description: '十二少AI绘图坊',
   repositoryUrl: 'https://github.com/sixiaopangai/gpt_image_playground',
+  repositoryLabel: '@sixiaopangai',
   releaseRepo: 'sixiaopangai/gpt_image_playground',
   defaultApiUrl: 'https://newapi.yushenchuanmei.de5.net',
 } as const
